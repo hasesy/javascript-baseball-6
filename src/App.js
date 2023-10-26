b01e885288660274eba96543788fc598379e5d9c
Seoyeon Kim <kseoy0607@gmail.com>
2023-10-26 21:31:04 +0900

    style: prettier 설정 변경 후 코드 포맷팅

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
 
     while (true) {
       await this.game();
@@ -10,7 +10,7 @@ class App {
       const answer = await this.menuRestartOrExit();
       this.menuCheckAndExitOnError(answer);
 
-      if (answer === "2") {
+      if (answer === '2') {
         break;
       }
     }
@@ -29,8 +29,8 @@ class App {
 
       MissionUtils.Console.print(result);
 
-      if (result === "3스트라이크") {
-        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      if (result === '3스트라이크') {
+        MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
         break;
       }
     }
@@ -52,14 +52,14 @@ class App {
       }
     }
 
-    const secret = randomUnipueNumbers.join("");
+    const secret = randomUnipueNumbers.join('');
     return secret;
   }
 
   // 사용자로부터 값을 입력받는 함수
   receiveNumber() {
     const receive =
-      MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+      MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
     return receive;
   }
 
@@ -76,8 +76,8 @@ class App {
 
   // 입력받은 수를 비교해서 결과 도출하는 함수
   compareNumbers(secret, receive) {
-    const secretNum = secret.split("");
-    const receiveNum = receive.split("");
+    const secretNum = secret.split('');
+    const receiveNum = receive.split('');
     let strikes = 0;
     let balls = 0;
 
@@ -97,31 +97,31 @@ class App {
     const count = this.compareNumbers(secret, receive);
 
     if (count[0] + count[1] === 0) {
-      return "낫싱";
+      return '낫싱';
     }
 
     if (count[0] === 0) {
-      return count[1] + "볼";
+      return count[1] + '볼';
     }
 
     if (count[1] === 0) {
-      return count[0] + "스트라이크";
+      return count[0] + '스트라이크';
     }
 
-    return count[1] + "볼 " + count[0] + "스트라이크";
+    return count[1] + '볼 ' + count[0] + '스트라이크';
   }
 
   // 게임 재시작 or 종료 값을 입력받는 함수
   menuRestartOrExit() {
     const answer = MissionUtils.Console.readLineAsync(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
     );
     return answer;
   }
 
   // 예외 처리하는 함수
   throwException() {
-    throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
   }
 
   // 입력받은 숫자 검증 후 종료하는 함수
@@ -130,7 +130,7 @@ class App {
       this.throwException();
     }
 
-    const arrayNum = receive.split("");
+    const arrayNum = receive.split('');
     const duplicate = new Set();
 
     for (let i = 0; i < arrayNum.length; i++) {
